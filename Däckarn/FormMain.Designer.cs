fd845b527beae37e4a8ba49d57fc44c013f52d47
Andreas Lindström <zetitup@gmail.com>
2025-01-03 10:17:26 +0100

    Title

--- Däckarn/FormMain.Designer.cs
+++ Däckarn/FormMain.Designer.cs
@@ -39,7 +39,7 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { arkivToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.Size = new Size(821, 24);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -61,11 +61,13 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(821, 576);
             Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
             MainMenuStrip = menuStrip1;
+            MaximizeBox = false;
             Name = "FormMain";
-            Text = "Form1";
+            Text = "Däckarn";
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
             ResumeLayout(false);
