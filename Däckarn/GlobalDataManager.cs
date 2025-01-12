24b69b287be2057dbfbdea08013c8c4409047cc4
AndreasL85 <h24andrl@du.se>
2025-01-12 14:22:05 +0100

    GDM

--- Däckarn/GlobalDataManager.cs
+++ Däckarn/GlobalDataManager.cs
@@ -8,4 +8,29 @@ using DackarnAPI.Services;
 
 namespace Däckarn
 {
+    public static class GlobalDataManager
+    {
+        private static int year = DateTime.Now.Year;
+        private static int month = DateTime.Now.Month;
+        private static int day = DateTime.Now.Day;
+        public static Schedule? WorkSchedule { get; private set; }
+        public static BookingManager BookingManager { get; private set; } = new BookingManager();
+
+        public static void Initialize()
+        {
+            BookingManager = new BookingManager();
+            WorkSchedule = new Schedule(new DateTime(year, month, day, 8, 0, 0), // Start hour
+                                        new DateTime(year, month, day, 17, 0, 0), // End hour
+                                        new DateTime(year, month, day, 13, 0, 0)); // Lunch hour 
+        }
+
+
+        public static void UpdateSchedule(DateTime date, bool available)
+        {
+            if (WorkSchedule != null)
+            {
+                WorkSchedule.UpdateJobTimeFrame(date, available);
+            }
+        }
+    }
 }
